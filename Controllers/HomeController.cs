a32908bfa3e5b0875617fc638d71b3d6f63c20e6
Santana Jolly <santanaharris@gmail.com>
2019-08-19 13:29:22 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PersonalWebsite.Models;
+
+namespace PersonalWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Profile");
+        }
+        public ViewResult Education()
+        {
+            return View();
+        }
+        public ViewResult Projects()
+        {
+            return View();
+        }
+        public ViewResult Skills()
+        {
+            return View();
+        }
+        public ViewResult Hobbies()
+        {
+            return View();
+        }
+        public ViewResult Experience()
+        {
+            return View();
+        }
+        public ViewResult Contact()
+        {
+            return View();
+        }
+
+
+    }
+}
